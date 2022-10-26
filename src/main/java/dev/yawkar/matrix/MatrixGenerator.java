8f287005cd6d3dfb71dc6555c56bc888f28553ed
yawkar <yawkarpov@gmail.com>
2022-10-26 15:14:18 +0300

    feat: add MatrixGenerator interface and impls

---
+++ src/main/java/dev/yawkar/matrix/MatrixGenerator.java
@@ -0,0 +1,6 @@
+package dev.yawkar.matrix;
+
+public interface MatrixGenerator {
+
+    public long[][] generate(int rowsNumber, int columnsNumber);
+}
---
+++ src/main/java/dev/yawkar/matrix/generator/AlphaGenerator.java
@@ -0,0 +1,17 @@
+package dev.yawkar.matrix.generator;
+
+import dev.yawkar.matrix.MatrixGenerator;
+
+public class AlphaGenerator implements MatrixGenerator {
+
+    @Override
+    public long[][] generate(int rowsNumber, int columnsNumber) {
+        long[][] matrix = new long[rowsNumber][columnsNumber];
+        for (int i = 0; i < rowsNumber; ++i) {
+            for (int j = 0; j < columnsNumber; ++j) {
+                matrix[i][j] = ((long) rowsNumber / columnsNumber * i + j) * 2;
+            }
+        }
+        return matrix;
+    }
+}
