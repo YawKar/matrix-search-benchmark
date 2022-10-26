df0cb55a627a10f08ed3bd4986ec8bbede9d561f
yawkar <yawkarpov@gmail.com>
2022-10-26 21:24:44 +0300

    feat: add main (lol)

--- src/main/java/dev/yawkar/Main.java
+++ src/main/java/dev/yawkar/Main.java
@@ -1,7 +1,15 @@
 package dev.yawkar;
 
+import dev.yawkar.benchmark.MatrixSearchBenchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Options options = new OptionsBuilder()
+                .include(MatrixSearchBenchmark.class.getSimpleName())
+                .build();
+        new Runner(options).run();
     }
 }
