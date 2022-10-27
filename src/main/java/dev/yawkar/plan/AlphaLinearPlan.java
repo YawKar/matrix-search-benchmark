ac4e3d825cd266bd1441d1a1ebb35689744cc52e
yawkar <yawkarpov@gmail.com>
2022-10-27 11:34:47 +0300

    refactor: simplify plans through inheritance

--- src/main/java/dev/yawkar/plan/AlphaLinearPlan.java
+++ src/main/java/dev/yawkar/plan/AlphaLinearPlan.java
@@ -3,16 +3,9 @@ package dev.yawkar.plan;
 import dev.yawkar.matrix.MatrixGenerator;
 import dev.yawkar.matrix.generator.AlphaGenerator;
 import dev.yawkar.plan.simple.SimpleLinearPlan;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
 
-@State(Scope.Benchmark)
 public class AlphaLinearPlan extends SimpleLinearPlan {
-
-    @Param({"2", "4", "8", "16", "32", "64", "128", "256", "512", "1024", "2048", "4096", "8192"})
-    public int rowsNumber;
 
     public MatrixGenerator generator = new AlphaGenerator();
 
--- src/main/java/dev/yawkar/plan/simple/SimplePlan.java
+++ src/main/java/dev/yawkar/plan/simple/SimplePlan.java
@@ -2,9 +2,14 @@ package dev.yawkar.plan.simple;
 
 import dev.yawkar.algorithm.SearchAlgorithm;
 import dev.yawkar.matrix.MatrixGenerator;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
+@State(Scope.Benchmark)
 public class SimplePlan {
 
+    @Param({"2", "4", "8", "16", "32", "64", "128", "256", "512", "1024", "2048", "4096", "8192"})
     public int rowsNumber;
 
     public int columnsNumber = 8192;
@@ -14,8 +19,4 @@ public class SimplePlan {
     public MatrixGenerator generator;
 
     public long[][] matrix;
-
-    public void setUp() {
-        matrix = generator.generate(rowsNumber, columnsNumber);
-    }
 }
